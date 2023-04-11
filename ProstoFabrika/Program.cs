0558d819aab8f584a7df35aab9a5475a348413ef
Влад <@DESKTOP-MIKGT6B>
2023-04-11 18:21:19 +0300

    Добавьте файлы проекта.

---
+++ ProstoFabrika/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProstoFabrika
+{
+    class Program
+    {
+        public abstract class Building
+        {
+            public string Material { get; protected set; }
+
+            public abstract void Build();
+        }
+        public class WoodenHouse : Building
+        {
+            public WoodenHouse()
+            {
+                Material = "Дерево";
+            }
+
+            public override void Build()
+            {
+                Console.WriteLine("Створено дерев'яний будинок");
+            }
+        }
+
+        public class StoneHouse : Building
+        {
+            public StoneHouse()
+            {
+                Material = "Камінь";
+            }
+
+            public override void Build()
+            {
+                Console.WriteLine("Створено кам'яний будинок");
+            }
+        }
+
+        public class BrickHouse : Building
+        {
+            public BrickHouse()
+            {
+                Material = "Цегла";
+            }
+
+            public override void Build()
+            {
+                Console.WriteLine("Створено цегляний будинок");
+            }
+        }
+        public interface IBuildingFactory
+        {
+            Building CreateBuilding();
+        }
+        public class WoodenHouseFactory : IBuildingFactory
+        {
+            public Building CreateBuilding()
+            {
+                return new WoodenHouse();
+            }
+        }
+
+        public class StoneHouseFactory : IBuildingFactory
+        {
+            public Building CreateBuilding()
+            {
+                return new StoneHouse();
+            }
+        }
+
+        public class BrickHouseFactory : IBuildingFactory
+        {
+            public Building CreateBuilding()
+            {
+                return new BrickHouse();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IBuildingFactory woodenHouseFactory = new WoodenHouseFactory();
+            IBuildingFactory stoneHouseFactory = new StoneHouseFactory();
+            IBuildingFactory brickHouseFactory = new BrickHouseFactory();
+
+            Building woodenHouse = woodenHouseFactory.CreateBuilding();
+            Building stoneHouse = stoneHouseFactory.CreateBuilding();
+            Building brickHouse = brickHouseFactory.CreateBuilding();
+
+            woodenHouse.Build();
+            stoneHouse.Build();
+            brickHouse.Build();
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ ProstoFabrika/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ProstoFabrika")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProstoFabrika")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("29e24b7e-1fc9-4851-b8ab-8d5524e27c92")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
